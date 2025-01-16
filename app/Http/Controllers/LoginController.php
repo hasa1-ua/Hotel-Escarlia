d30415ff46d35fa48f7bf0c9743b59e2bdeefe85
hasa1 <hasa1@alu.ua.es>
2025-01-16 23:18:09 +0100

    Coockies a la hora de cerrar sesion

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 
 class LoginController extends Controller
 {
@@ -43,6 +44,8 @@ class LoginController extends Controller
     public function logout()
     {
         Auth::logout();
-        return redirect('/login');
+
+        $cookie = Cookie::forget('laravel_session');
+        return redirect('/login')->withCookie($cookie);
     }
 }
